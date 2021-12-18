34890c879edd893a115a0a35c8131730def7ca10
Ben Lugavere <b.lugavere@gmail.com>
2021-12-18 12:33:21 -0800

    refactor sidebar types to be more dry, rename sidebar label to info.title else filename

--- packages/docusaurus-plugin-openapi/src/sidebars/index.ts
+++ packages/docusaurus-plugin-openapi/src/sidebars/index.ts
@@ -5,6 +5,8 @@
  * LICENSE file in the root directory of this source tree.
  * ========================================================================== */
 
+import path from "path";
+
 import { ApiPageMetadata } from "../types";
 
 interface Options {
@@ -19,29 +21,31 @@ type NavbarItem = {
   position?: "left" | "right";
 } & Record<string, unknown>;
 
-type Item =
-  | {
-      [key: string]: any;
-      type: "info";
-      info: any;
-      title: string;
-      permalink: string;
-      id: string;
-    }
-  | {
-      [key: string]: any;
-      type: "api";
-      api: {
-        // todo: include info
-        // info: {
-        // title: string;
-        // },
-        tags?: string[] | undefined;
-      };
-      title: string;
-      permalink: string;
-      id: string;
-    };
+export type BaseItem = {
+  [key: string]: any;
+  title: string;
+  permalink: string;
+  id: string;
+  source: string;
+};
+
+export type InfoItem = BaseItem & {
+  type: "info";
+  info: any;
+};
+
+export type ApiItem = BaseItem & {
+  type: "api";
+  api: {
+    // todo: include info
+    // info: {
+    // title: string;
+    // },
+    tags?: string[] | undefined;
+  };
+};
+
+type Item = InfoItem | ApiItem;
 
 function groupByTags(
   items: Item[],
@@ -125,6 +129,9 @@ function groupByTags(
         })
         .map((item) => {
           return {
+            source: item.source,
+            info: item.api.info,
+
             type: "link",
             label: item.title,
             href: item.permalink,
@@ -135,7 +142,19 @@ function groupByTags(
           };
         }),
     },
-  ];
+  ].map((categoryItem) => {
+    const [prototype] = categoryItem.items;
+    if (!prototype) {
+      return categoryItem;
+    }
+    const { info } = prototype;
+    const fileName = path.basename(prototype.source).split(".")[0];
+
+    return {
+      ...categoryItem,
+      label: info?.title ?? fileName,
+    };
+  });
 
   return [...intros, ...tagged, ...untagged];
 }
